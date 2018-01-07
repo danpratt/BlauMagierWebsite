19295232a7985518d593d64779d02037aab3db6b
Daniel Pratt <daniel@blaumagier.com>
2018-01-06 21:29:26 -0800

    feat: enabled e-mail firebase cloud functions and made style changes
    - enabled e-mail cloud function so that i receive e-mail anytime someone sends me a message
    - made style changes to navigation
    - enabled some links on blog

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,47 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
-// });
+// });'
+'use strict';
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
+
+// Sends an email confirmation when a user changes his mailing list subscription.
+exports.sendEmailConfirmation = functions.database.ref('/messages/{uid}').onWrite(event => {
+  const snapshot = event.data;
+  const val = snapshot.val();
+
+  const name = val.name;
+  const message = val.message;
+  const email = val.email;
+
+  const message_to_send = 'Hi Daniel,\n\nYou have a message from: ' + name + '\nemail: ' + email + '\nmessage: ' + message;
+
+  const mailOptions = {
+    from: '"Firebase Realtime Database for Blau Magier" <noreply@firebase.com>',
+    to: 'daniel@blaumagier.com'
+  };
+  
+  // Building Email message.
+  mailOptions.subject = 'New Message on Firebase';
+  mailOptions.text = message_to_send;
+  
+  return mailTransport.sendMail(mailOptions)
+    .then(() => console.log(`New message from: `, val.email))
+    .catch(error => console.error('There was an error while sending the email:', error));
+});
